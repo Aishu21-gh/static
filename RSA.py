d75cd5f03ac08b11b30ab3ebb4f66e637ad1b781
Aishu21-gh <aishwaryagh273@gmail.com>
2025-12-01 07:30:05 +0530

    Add files via upload

---
+++ RSA.py
@@ -0,0 +1,32 @@
+import math
+def gcd(a,h):
+    temp = 0
+    while(1):
+         temp = a % h
+         if (temp ==0):
+            return h
+         a = h
+         h = temp
+p=int(input("enter the p value"))
+q=int(input("enter the q value"))
+n = p*q
+e = 2
+phi =(p-1)*(q-1)
+while (e <phi):
+    if(gcd(e, phi) == 1):
+        break
+    else:
+        e = e+1
+k = 2
+d = (1 + (k*phi))/e
+
+msg = 12
+print("message data = ",msg)
+c = pow(msg, e)
+c = math.fmod(c, n)
+print("encrypted data = ", c)
+
+m =pow(c,d)
+m = math.fmod(m ,n)
+print("original message sent = ", m)
+    
